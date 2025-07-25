f2b9d014b80ebe3d30d7d6728801198dfa40a0eb
Ganning Xu <ganningxu@gmail.com>
2025-07-25 13:54:59 -0700

    add events page for allen workshop (#29)
    * add events page for allen workshop
    
    * update instructions

--- airavata-research-portal/src/components/events/index.tsx
+++ airavata-research-portal/src/components/events/index.tsx
@@ -1,32 +1,38 @@
-import { Container, Accordion, Spacer, Span } from "@chakra-ui/react";
-import { Apr11Workshop } from "./Apr11Workshop";
-import { May7Workshop } from "./May7Workshop";
+import {Accordion, Container, Spacer, Span} from "@chakra-ui/react";
+import {Apr11Workshop} from "./Apr11Workshop";
+import {May7Workshop} from "./May7Workshop";
+import {Jul28AllenWorkshop} from "@/components/events/Jul28AllenWorkshop.tsx";
 
 export const Events = () => {
   return (
-    <Container maxW="breakpoint-lg" my={8}>
-      <Accordion.Root multiple defaultValue={["May 7, 2025"]}>
-        {events.map((event) => (
-          <Accordion.Item key={event.id} value={event.id} mb={8}>
-            <Accordion.ItemTrigger>
-              {event.name} <Spacer />
-              <Span color="gray.400" fontSize="sm">
-                {event.id}
-              </Span>
-              <Accordion.ItemIndicator />
-            </Accordion.ItemTrigger>
+      <Container maxW="breakpoint-lg" my={8}>
+        <Accordion.Root multiple defaultValue={[events[0].id]}>
+          {events.map((event) => (
+              <Accordion.Item key={event.id} value={event.id} mb={8}>
+                <Accordion.ItemTrigger>
+                  {event.name} <Spacer/>
+                  <Span color="gray.400" fontSize="sm">
+                    {event.id}
+                  </Span>
+                  <Accordion.ItemIndicator/>
+                </Accordion.ItemTrigger>
 
-            <Accordion.ItemContent>
-              <Accordion.ItemBody>{event.component()}</Accordion.ItemBody>
-            </Accordion.ItemContent>
-          </Accordion.Item>
-        ))}
-      </Accordion.Root>
-    </Container>
+                <Accordion.ItemContent>
+                  <Accordion.ItemBody>{event.component()}</Accordion.ItemBody>
+                </Accordion.ItemContent>
+              </Accordion.Item>
+          ))}
+        </Accordion.Root>
+      </Container>
   );
 };
 
 const events = [
+  {
+    id: "July 28-30, 2025",
+    name: "2025 Allen Institute Modeling Software Workshop (28-30 July 2025)",
+    component: Jul28AllenWorkshop
+  },
   {
     id: "May 7, 2025",
     name: "Cyberinfrastructure and Services for Science & Engineering Workshop",
@@ -36,5 +42,5 @@ const events = [
     id: "April 11, 2025",
     name: "Data-Driven and Large-Scale Modeling in Neuroscience",
     component: Apr11Workshop,
-  },
+  }
 ];
--- airavata-research-portal/src/components/typography/KeyPair.tsx
+++ airavata-research-portal/src/components/typography/KeyPair.tsx
@@ -1,34 +1,35 @@
-import { Text } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/react";
+import {Link, Text} from "@chakra-ui/react";
 
 export const KeyPair = ({
-  keyStr,
-  valueStr,
-}: {
+                          keyStr,
+                          valueStr,
+                          href
+                        }: {
   keyStr: string;
   valueStr: string;
+  href?: string;
 }) => {
-  const isLink = valueStr.startsWith("http");
+  const isLink = valueStr.startsWith("http") || (href != null);
   return (
-    <Text>
-      <Text as="span" fontWeight="bold">
-        {keyStr}:{" "}
-      </Text>
-
-      {isLink ? (
-        <Link
-          href={valueStr}
-          target="_blank"
-          color="blue.600"
-          fontWeight="normal"
-        >
-          {valueStr}
-        </Link>
-      ) : (
-        <Text as="span" fontWeight="normal">
-          {valueStr}
+      <Text>
+        <Text as="span" fontWeight="bold">
+          {keyStr}:{" "}
         </Text>
-      )}
-    </Text>
+
+        {isLink ? (
+            <Link
+                href={href}
+                target="_blank"
+                color="blue.600"
+                fontWeight="normal"
+            >
+              {valueStr}
+            </Link>
+        ) : (
+            <Text as="span" fontWeight="normal">
+              {valueStr}
+            </Text>
+        )}
+      </Text>
   );
 };
